ea213ce3efc25d11e5147c0a0afd480c3b2bb429
LuxesIT <ot@luxes.es>
2026-03-24 11:42:25 +0100

    add delay

--- src/Panel.java
+++ src/Panel.java
@@ -224,6 +224,8 @@ class Panel {
                     new ProcessBuilder("wmctrl", "-a", title).start().waitFor();
                     new ProcessBuilder("wmctrl", "-r", title, "-b", "remove,hidden").start().waitFor();
 
+                    Thread.sleep(150);
+
                     if (topBar != null) {
                         Thread.sleep(300);
                         SwingUtilities.invokeLater(() -> {
